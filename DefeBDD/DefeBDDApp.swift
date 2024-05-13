daa433e9ef1023decb25155e5a2fe129d781b619
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-05-13 20:38:30 -0300

    Initial Commit

---
+++ DefeBDD/DefeBDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefeBDDApp.swift
+//  DefeBDD
+//
+//  Created by Federico Diaz Nemeth on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DefeBDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
